02055e546a2c8dd3b6439f39bf7bbefeace16a09
Emina Ergul <eminaergul712@gmail.com>
2022-01-13 23:57:14 +0000

    quotes setup

---
+++ script.js
@@ -0,0 +1,11 @@
+// const api ="https://type.fit/api/quotes";
+
+// async function getapi(url)
+// {
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   console.log(data);
+// }
+
+// getapi(api);
+
