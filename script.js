25897f4d503fd1981ed8297367c93ff51b69a018
Emina Ergul <eminaergul712@gmail.com>
2022-01-14 01:05:43 +0000

    menu functioning

--- script.js
+++ script.js
@@ -9,3 +9,13 @@
 
 // getapi(api);
 
+const menuBtn = document.querySelector(".menu-btn")
+const closeMenuBtn = document.querySelector(".close-menu-btn")
+
+menuBtn.addEventListener("click", () => {
+    document.querySelector(".menu-panel").style.display = "inline"
+})
+
+closeMenuBtn.addEventListener("click", () => {
+    document.querySelector(".menu-panel").style.display = "none"
+})
